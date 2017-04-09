55597b9e63088e6898040338dde87405d4fd1a6d
Shtereva <a.i.shtereva@gmail.com>
2017-04-09 18:37:20 +0300

    Programming Fundamentals with CSharp

---
+++ DataTypes/RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorSpecialNumbers
+{
+    class RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                int currentNumber = i;
+                int sum = 0;
+                while (currentNumber > 0)
+                {
+                    sum += currentNumber % 10;
+                    currentNumber = currentNumber / 10;
+                }
+                bool result = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {result}");
+            }
+
+        }
+    }
+}
