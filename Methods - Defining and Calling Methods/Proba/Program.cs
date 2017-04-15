651a8ce6b8d9a988bee7ce116386f29c4da7a18d
Shtereva <a.i.shtereva@gmail.com>
2017-04-15 21:25:55 +0300

    Programming Fundamentals with CSharp

---
+++ Methods - Defining and Calling Methods/Proba/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printnumbers(5, 10);
+            Printnumbers(15);
+            Printnumbers();
+            Printnumbers(start:35, end: 55);
+        }
+
+        private static void Printnumbers(int start = 0, int end = 100)
+        {
+            for (int i = start; i < end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Methods - Defining and Calling Methods/SignOfIntege Number/SignOfIntegerNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfIntege_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int n = int.Parse(Console.ReadLine());
+            PrintSign(n);
+        }
+        static void PrintSign(int number)
+        {
+            if (number > 0) { Console.WriteLine("The number {0} is positive.", number); }
+            else if (number < 0) { Console.WriteLine("The number {0} is negative.", number); }
+            else { Console.WriteLine("The number {0} is zero.", number); }
+        }
+    }
+}
