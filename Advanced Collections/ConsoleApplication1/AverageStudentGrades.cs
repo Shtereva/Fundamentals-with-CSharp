e6a45985b093104d1c5e2b79b708ca9d7260ae42
Shtereva <a.i.shtereva@gmail.com>
2017-05-30 23:25:17 +0300

    Programing Fundamentals with CSharp

---
+++ Advanced Collections/ConsoleApplication1/AverageStudentGrades.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+class AverageStudentGrades
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var studentRecord = new Dictionary<string, List<double>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var students = Console.ReadLine().Split();
+            string name = students[0];
+            double grade = double.Parse(students[1]);
+
+
+            if (!studentRecord.ContainsKey(name))
+            {
+                studentRecord.Add(name, new List<double>());
+            }
+
+            studentRecord[name].Add(grade);
+        }
+
+        foreach (var name in studentRecord)
+        {
+            Console.WriteLine($"{name.Key} -> {string.Join(" ", name.Value.Select(x => string.Format("{0:f2}", x)))} (avg: {name.Value.Average():f2})");
+        }
+    }
+}
+
---
+++ Advanced Collections/ConsoleApplication2/CitiesByContinentAndCountry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+    class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var CitiesByContinentAndCountry = new Dictionary<string, Dictionary<string, List<string>>>();
+                                                     // continent         //country    //cities   
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+            string continent = input[0];
+            string country = input[1];
+            string city = input[2];
+            AddingContinentsCountriesAndCities(CitiesByContinentAndCountry, continent, country, city);
+        }
+
+        foreach (var continent in CitiesByContinentAndCountry.Keys)
+        {
+            Console.WriteLine($"{continent}:");
+
+            foreach (var country in CitiesByContinentAndCountry[continent])
+            {
+                Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+            }
+        }
+    }
+
+    private static void AddingContinentsCountriesAndCities(Dictionary<string, Dictionary<string, List<string>>> CitiesByContinentAndCountry, string continent, string country, string city)
+    {
+        if (!CitiesByContinentAndCountry.ContainsKey(continent))
+        {
+            CitiesByContinentAndCountry[continent] = new Dictionary<string, List<string>>();
+        }
+
+        if (!CitiesByContinentAndCountry[continent].ContainsKey(country))
+        {
+            CitiesByContinentAndCountry[continent][country] = new List<string>();
+        }
+            CitiesByContinentAndCountry[continent][country].Add(city);
+    }
+}
+
