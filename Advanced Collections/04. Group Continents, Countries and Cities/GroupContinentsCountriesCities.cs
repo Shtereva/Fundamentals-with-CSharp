f27812897db11624ca2d3d9abd09de007e8305bd
Shtereva <a.i.shtereva@gmail.com>
2017-05-31 17:39:42 +0300

    Programming Fundamentals with CSharp

---
+++ Advanced Collections/04. Group Continents, Countries and Cities/GroupContinentsCountriesCities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var CitiesByContinentAndCountry = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+        // continent         //country    //cities   
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+            string continent = input[0];
+            string country = input[1];
+            string city = input[2];
+            AddingContinentsCountriesAndCities(CitiesByContinentAndCountry, continent, country, city);
+        }
+
+        foreach (var continent in CitiesByContinentAndCountry.Keys)
+        {
+            Console.WriteLine($"{continent}:");
+
+            foreach (var country in CitiesByContinentAndCountry[continent])
+            {
+                Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+            }
+        }
+    }
+
+    private static void AddingContinentsCountriesAndCities(SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> CitiesByContinentAndCountry, string continent, string country, string city)
+    {
+        if (!CitiesByContinentAndCountry.ContainsKey(continent))
+        {
+            CitiesByContinentAndCountry[continent] = new SortedDictionary<string, SortedSet<string>>();
+        }
+
+        if (!CitiesByContinentAndCountry[continent].ContainsKey(country))
+        {
+            CitiesByContinentAndCountry[continent][country] = new SortedSet<string>();
+        }
+        CitiesByContinentAndCountry[continent][country].Add(city);
+    }
+}
+
---
+++ Advanced Collections/ConsoleApplication3/RecordUniqueNames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RecordUniqueNames
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var listOfNames = new SortedSet<string>(); 
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            listOfNames.Add(line);
+        }
+
+        Console.WriteLine(string.Join(Environment.NewLine, listOfNames));
+    }
+}
