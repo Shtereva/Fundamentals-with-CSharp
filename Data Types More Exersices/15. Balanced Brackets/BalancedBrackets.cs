430d992d2a183094ab72ab7a9bb4ddd98030bee3
Shtereva <a.i.shtereva@gmail.com>
2017-06-03 17:12:25 +0300

    Programming Fundamentals with CSharp

---
+++ Data Types More Exersices/15. Balanced Brackets/BalancedBrackets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class BalancedBrackets
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var brackets = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            if (line == "(" || line == ")")
+            {
+                brackets += line;
+            }
+        }
+
+        bool isBalanced = true;
+
+        for (int i = 0; i < brackets.Length - 1; i += 2)
+        {
+            if (brackets[i] == '(' && brackets[i + 1] == ')' && brackets.Length % 2 == 0)
+            {
+                continue;
+            }
+
+            else
+            {
+                isBalanced = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isBalanced ? "BALANCED" : "UNBALANCED");
+    }
+}
