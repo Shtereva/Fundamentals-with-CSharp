04642c99cf72666ac0ede0902e1a8b5e23470a70
Shtereva <a.i.shtereva@gmail.com>
2017-06-04 19:02:05 +0300

    Programming Fundamentals with CSharp

---
+++ Lambda and LINQ/01. Register Users/RegisterUsers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+class RegisterUsers
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+        var users = new Dictionary<string, DateTime>();
+
+        while (input[0] != "end")
+        {
+            string username = input[0];
+            var date = DateTime.ParseExact(input[1], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            users[username] = date;
+            input = Console.ReadLine().Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        var orderedUsers = users
+            .Reverse()
+            .OrderBy(x => x.Value)
+            .Take(5)
+            .OrderByDescending(x => x.Value)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var kvp in orderedUsers)
+        {
+            Console.WriteLine(kvp.Key);
+        }
+    }
+}
+
