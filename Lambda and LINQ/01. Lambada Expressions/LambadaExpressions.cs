2554da0cfa7275b50660ca8e71bf6206b58aec62
Shtereva <a.i.shtereva@gmail.com>
2017-06-06 18:07:50 +0300

    Programming Fundamentals with CSharp

--- Lambda and LINQ/01. Lambada Expressions/LambadaExpressions.cs
+++ Lambda and LINQ/01. Lambada Expressions/LambadaExpressions.cs
@@ -8,56 +8,55 @@ class LambadaExpressions
         string input = Console.ReadLine();
         var dict = new Dictionary<string, List<string>>();
 
-        string previousProperty = string.Empty;
-        string previousObject = string.Empty;
-        string previousSelector = string.Empty;
+        string property = string.Empty;
+        string selectorObject = string.Empty;
+        string selector = string.Empty;
+
         while (input != "lambada")
         {
             if (input == "dance")
             {
-                dict[previousSelector].Add(previousObject + ".");
+                foreach (var item in dict)
+                {
+                    dict[item.Key].Insert(0, item.Value[0]);
+                }
+
                 input = Console.ReadLine();
                 continue;
             }
 
             var line = input.Split(new[] { ' ', '=', '>', '.' }, StringSplitOptions.RemoveEmptyEntries);
-            string selector = line[0];
-            string selectorObject = line[1];
-            string property = line[2];
-            AddingExpressionsToDictionary(dict, previousProperty, selector, selectorObject, property);
-
-            previousProperty = property;
-            previousObject = selectorObject;
-            previousSelector = selector;
-
+            selector = line[0];
+            selectorObject = line[1];
+            property = line[2];
+            AddingExpressionsToDictionary(dict, property, selector, selectorObject);
             input = Console.ReadLine();
         }
 
+
         foreach (var item in dict)
         {
-            Console.Write($"{item.Key} => ");
+            //var rightPart = item.Value.ToList();
 
-           // var rightPart = item.Value
-                //.OrderBy()
-                //.to
+            Console.WriteLine($"{item.Key} => {string.Join(".", item.Value)}");
         }
     }
 
-    private static void AddingExpressionsToDictionary(Dictionary<string, List<string>> dict, string previousProperty, string selector, string selectorObject, string property)
+    private static void AddingExpressionsToDictionary(Dictionary<string, List<string>> dict, string property, string selector, string selectorObject)
     {
         if (!dict.ContainsKey(selector))
         {
             dict[selector] = new List<string>();
         }
 
-        if (dict[selector].Contains(selectorObject + "."))
+        if (dict[selector].Contains(selectorObject))
         {
-            dict[selector].Remove(previousProperty);
+            dict[selector].RemoveAt(dict[selector].Count - 1);
         }
 
         else
         {
-            dict[selector].Add(selectorObject + ".");
+            dict[selector].Add(selectorObject);
         }
         dict[selector].Add(property);
     }
