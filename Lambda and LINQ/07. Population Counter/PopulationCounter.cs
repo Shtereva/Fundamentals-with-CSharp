93a32dcec012d31d1a84ada775d0a2ac29a96d58
Shtereva <a.i.shtereva@gmail.com>
2017-06-09 22:12:18 +0300

    Programming Fundamentals with CSharp

---
+++ Lambda and LINQ/07. Population Counter/PopulationCounter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class PopulationCounter
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        var statistic = new Dictionary<string, Dictionary<string, long>>();
+
+        while (input != "report")
+        {
+            var line = input.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            string country = line[1];
+            string city = line[0];
+            int population = int.Parse(line[2]);
+
+            if (!statistic.ContainsKey(country))
+            {
+                statistic[country] = new Dictionary<string, long>();
+            }
+            statistic[country][city] = population;
+
+            input = Console.ReadLine();
+        }
+
+        statistic = statistic
+            .OrderByDescending(country => country.Value.Sum(city => city.Value))
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var country in statistic)
+        {
+            Console.WriteLine($"{country.Key} (total population: {country.Value.Sum(x => x.Value)})");
+
+            country.Value
+                .OrderByDescending(x => x.Value)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"=>{x.Key}: {x.Value}"));
+            
+        }            
+    }
+}
---
+++ Lambda and LINQ/08. Logs Aggregator/LogsAggregator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class LogsAggregator
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var logs = new SortedDictionary<string, SortedDictionary<string, int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+            string name = input[1];
+            string addressIP = input[0];
+            int duration = int.Parse(input[2]);
+
+            AddDataToLogs(logs, name, addressIP, duration);
+        }
+
+        foreach (var name in logs)
+        {
+            Console.Write($"{name.Key}: {name.Value.Sum(x => x.Value)} ");
+
+          var result =  name.Value
+                .Select(x => x.Key)
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine($"[{string.Join(", ", result)}]");
+        }
+    }
+
+    private static void AddDataToLogs(SortedDictionary<string, SortedDictionary<string, int>> logs, string name, string addressIP, int duration)
+    {
+        if (!logs.ContainsKey(name))
+        {
+            logs[name] = new SortedDictionary<string, int>();
+        }
+
+        if (!logs[name].ContainsKey(addressIP))
+        {
+            logs[name][addressIP] = 0;
+        }
+
+        logs[name][addressIP] += duration;
+    }
+}
