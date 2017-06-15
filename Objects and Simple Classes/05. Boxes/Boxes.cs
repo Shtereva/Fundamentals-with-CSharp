4fa00ecd65b64346929e00e2dfc06eabcf26d8e1
Shtereva <a.i.shtereva@gmail.com>
2017-06-15 23:53:06 +0300

    Programming Fundamentals with CSharp

---
+++ Objects and Simple Classes/05. Boxes/Boxes.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Boxes
+{
+    public class Box
+    {
+
+        public static int CalculatePerimeter(int width, int height)
+        {
+            return (2 * width) + (2 * height);
+        }
+
+        public static int CalculateArea(int width, int height)
+        {
+            return width * height;
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static double Distance(Point firsPoint, Point secondPoint)
+        {
+            double sideA = firsPoint.X - secondPoint.X;
+            double sideB = firsPoint.Y - secondPoint.Y;
+            double distance = Math.Sqrt((sideA * sideA) + (sideB * sideB));
+
+            return distance;
+        }
+    }
+
+    public class Boxes
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+
+            while (input != "end")
+            {
+                var line = input.Split(" |:".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                Point firstPoint, secondPoint;
+
+                UpperLeftAndRightPoint(line, out firstPoint, out secondPoint);
+                double width = Point.Distance(firstPoint, secondPoint);
+
+                UpperAndBottomLeft(line, out firstPoint, out secondPoint);
+                double height = Point.Distance(firstPoint, secondPoint);
+
+                int perimeter = Box.CalculatePerimeter((int)width, (int)height);
+                int area = Box.CalculateArea((int)width, (int)height);
+
+                Console.WriteLine($"Box: {width}, {height}");
+                Console.WriteLine($"Perimeter: {perimeter}");
+                Console.WriteLine($"Area: {area}");
+
+                input = Console.ReadLine();
+            }
+
+            
+        }
+
+        private static void UpperAndBottomLeft(string[] line, out Point firstPoint, out Point secondPoint)
+        {
+            firstPoint = new Point
+            {
+                X = int.Parse(line[0]),
+                Y = int.Parse(line[1])
+            };
+            secondPoint = new Point
+            {
+                X = int.Parse(line[4]),
+                Y = int.Parse(line[5])
+            };
+        }
+
+        private static void UpperLeftAndRightPoint(string[] line, out Point firstPoint, out Point secondPoint)
+        {
+            firstPoint = new Point
+            {
+                X = int.Parse(line[0]),
+                Y = int.Parse(line[1])
+            };
+            secondPoint = new Point
+            {
+                X = int.Parse(line[2]),
+                Y = int.Parse(line[3])
+            };
+        }
+    }
+}
---
+++ Objects and Simple Classes/06. Messages/Messages.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Messages
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<string> ReceivedMessages { get; set; }
+
+        public static User RegisterUser(string input)
+        {
+            var line = input.Split();
+
+            return new User
+            {
+                Username = line[1]
+            };
+        }
+
+    }
+
+    public class Message
+    {
+        public string Content { get; set; }
+        public string Sender { get; set; }
+
+        public static Message ReadMessage(string input)
+        {
+            var line = input.Split();
+
+            return new Message
+            {
+                Sender = line[0],
+                Content = line[line.Length - 1]
+            };
+        }
+    }
+
+    public class Messages
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var users = new Dictionary<string, User>();
+            var username = new User();
+
+            while (input != "exit")
+            {
+                if (input.Contains("register"))
+                {
+                     username = User.RegisterUser(input);
+
+                    users[username.Username] = new User()
+                    {
+                        Username = username.Username
+                    };
+                }
+
+                else if (input.Contains("send"))
+                {
+                    var line = input.Split();
+
+                    if (!users.ContainsKey(line[0]) || !users.ContainsKey(line[2]))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+
+                    var message = Message.ReadMessage(input);
+                    username.ReceivedMessages = new List<string>();
+                    username.ReceivedMessages.Add(message.Content);
+
+                    users[line[2]].ReceivedMessages.Add(message.Content);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        
+    }
+}
