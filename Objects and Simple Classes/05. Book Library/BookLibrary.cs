86acf9d7a0d01b467b92c7cfd3fa2253e8dedc7c
Shtereva <a.i.shtereva@gmail.com>
2017-06-17 17:51:21 +0300

    Programming Fundamentals with CSharp

---
+++ Objects and Simple Classes/05. Book Library/BookLibrary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Book_Library
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string ReleaseDate { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+
+        public static Book Read(string[] line)
+        {
+            return new Book
+            {
+                Title = line[0],
+                Author = line[1],
+                Publisher = line[2],
+                ReleaseDate = line[3],
+                ISBN = line[4],
+                Price = decimal.Parse(line[5])
+            };
+        }
+    }
+
+    public class Library
+    {
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+    public class BookLibrary
+    {
+        static void Main()
+        {
+
+
+            int n = int.Parse(Console.ReadLine());
+            var result = new List<Library>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split();
+
+                var books = Book.Read(line);
+                var library = new Library();
+
+
+                library.Books = new List<Book>();
+                library.Name = books.Author;
+                library.Books.Add(books);
+
+                if (!result.Any(x => x.Name == books.Author))
+                {
+                    result.Add(library);
+                }
+
+                else
+                {
+
+                    for (int j = 0; j < result.Count; j++)
+                    {
+                        if (result[j].Name == books.Author)
+                        {
+                            result[j].Books[0].Price += books.Price;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            result
+                .SelectMany(x => x.Books)
+                .OrderByDescending(x => x.Price)
+                .ThenBy(x => x.Author)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x.Author} -> {x.Price:f2}"));
+        }
+    }
+}
---
+++ Objects and Simple Classes/06. Book Library Modification/BookLibraryModification.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace _06.Book_Library_Modification
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+
+        public static Book Read(string[] line)
+        {
+            return new Book
+            {
+                Title = line[0],
+                Author = line[1],
+                Publisher = line[2],
+                ReleaseDate =  DateTime.ParseExact(line[3], "dd.MM.yyyy", CultureInfo.InvariantCulture),
+                ISBN = line[4],
+                Price = decimal.Parse(line[5])
+            };
+        }
+    }
+
+    public class Library
+    {
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+    class BookLibraryModification
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var result = new List<Library>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split();
+
+                var books = Book.Read(line);
+                var library = new Library();
+
+
+                library.Books = new List<Book>();
+                library.Name = books.Author;
+                library.Books.Add(books);
+
+                if (!result.Any(x => x.Name == books.Author))
+                {
+                    result.Add(library);
+                }
+
+                else
+                {
+
+                    for (int j = 0; j < result.Count; j++)
+                    {
+                        if (result[j].Name == books.Author)
+                        {
+                            result[j].Books.Add(books);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var date = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            result
+                .SelectMany(x => x.Books)
+                .Where(x => x.ReleaseDate > date)
+                .OrderBy(x => x.ReleaseDate)
+                .ThenBy(x => x.Title)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x.Title} -> {x.ReleaseDate:dd.MM.yyyy}"));
+                
+                
+
+        }
+    }
+}
