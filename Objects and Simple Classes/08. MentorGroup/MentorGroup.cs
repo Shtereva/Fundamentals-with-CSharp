f31e1cfcbfa18b78f8ad55afe243bb7458f022c5
Shtereva <a.i.shtereva@gmail.com>
2017-06-17 22:02:21 +0300

    Programming Fundamentals with CSharp

---
+++ Objects and Simple Classes/08. MentorGroup/MentorGroup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace _08.MentorGroup
+{
+    public class MentorGroup
+    {
+        public class Student
+        {
+            public List<string> Comments { get; set; }
+
+            public List<DateTime> Dates { get; set; }
+        }
+
+        static void Main()
+        {
+            var mentorsGroup = new SortedDictionary<string, Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end of dates")
+            {
+                AddStudentsToGroup(mentorsGroup, input);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "end of comments")
+            {
+                var line = input.Split("-".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                if (!mentorsGroup.ContainsKey(line[0]))
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                mentorsGroup[line[0]].Comments.Add(line[1]);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var student in mentorsGroup)
+            {
+                Console.WriteLine(student.Key);
+                Console.WriteLine("Comments:");
+                student.Value.Comments
+                    .ToList()
+                    .ForEach(x => Console.WriteLine($"- {x}"));
+
+                Console.WriteLine("Dates attended:");
+
+                student.Value.Dates
+                    .OrderBy(x => x)
+                    .ToList()
+                    .ForEach(x => Console.WriteLine($"-- {x:dd/MM/yyyy}"));
+            }
+        }
+
+        private static void AddStudentsToGroup(SortedDictionary<string, Student> mentorsGroup, string input)
+        {
+            var line = input.Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string name = line[0];
+            var dates = line.Skip(1)
+                .Select(x => DateTime.ParseExact(x, "dd/MM/yyyy", CultureInfo.InvariantCulture))
+                .ToList();
+
+            if (!mentorsGroup.ContainsKey(name))
+            {
+                mentorsGroup[name] = new Student();
+                mentorsGroup[name].Dates = new List<DateTime>();
+                mentorsGroup[name].Comments = new List<string>();
+            }
+            mentorsGroup[name].Dates.AddRange(dates);
+        }
+    }
+}
---
+++ Objects and Simple Classes/7. Andrey and billiard/AndreyAndbilliard.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.Andrey_and_billiard
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public Dictionary<string, int> ShopList { get; set; }
+
+        public decimal Bill { get; set; }
+
+        public static Customer Read(string name)
+        {
+            return new Customer
+            {
+                Name = name,
+                ShopList = new Dictionary<string, int>(),
+                Bill = 0
+            };
+        }
+    }
+
+    class AndreyAndbilliard
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            var priceList = new Dictionary<string, decimal>();
+            AddPriceList(input, priceList);
+
+            input = Console.ReadLine();
+            var students = new List<Customer>();
+            int count = 0;
+
+            while (input != "end of clients")
+            {
+                var line = input.Split("-,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                string name = line[0];
+                string product = line[1];
+                int quantity = int.Parse(line[2]);
+
+                if (!priceList.ContainsKey(product))
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                var person = Customer.Read(name);
+
+                person.ShopList[product] = quantity;
+                person.Bill = quantity * priceList[product];
+
+                AddStudents(priceList, students, name, product, quantity, person);
+
+                input = Console.ReadLine();
+            }
+
+            PrintStudents(students);
+        }
+
+        private static void PrintStudents(List<Customer> students)
+        {
+            foreach (var student in students.OrderBy(x => x.Name))
+            {
+                Console.WriteLine(student.Name);
+                student.ShopList
+                    .ToList()
+                    .ForEach(x => Console.WriteLine($"-- {x.Key} - {x.Value}"));
+                Console.WriteLine($"Bill: {student.Bill:f2}");
+            }
+            var total = students.Select(x => x.Bill).ToList();
+
+            Console.WriteLine($"Total bill: {total.Sum():f2}");
+        }
+
+        private static void AddStudents(Dictionary<string, decimal> priceList, List<Customer> students, string name, string product, int quantity, Customer person)
+        {
+            if (!students.Any(x => x.Name == name))
+            {
+                students.Add(person);
+            }
+
+            else
+            {
+                for (int i = 0; i < students.Count; i++)
+                {
+                    if (students[i].Name == name)
+                    {
+                        if (!students[i].ShopList.ContainsKey(product))
+                        {
+                            students[i].ShopList[product] = 0;
+                        }
+
+                        students[i].ShopList[product] += quantity;
+                        students[i].Bill += quantity * priceList[product];
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void AddPriceList(string input, Dictionary<string, decimal> priceList)
+        {
+            for (int i = 0; i < int.Parse(input); i++)
+            {
+                var line = Console.ReadLine().Split("-".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                string product = line[0];
+                decimal price = decimal.Parse(line[1]);
+
+                priceList[product] = price;
+            }
+        }
+    }
+}
