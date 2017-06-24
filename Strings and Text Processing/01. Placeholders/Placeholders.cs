bba0e9f4980b16166563afabbfd585ac8a56edd5
Shtereva <a.i.shtereva@gmail.com>
2017-06-24 21:13:54 +0300

    Programming Fundamentals with CSharp

---
+++ Strings and Text Processing/01. Placeholders/Placeholders.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.Placeholders
+{
+    class Placeholders
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var a = input.IndexOf("{}");
+
+            while (input != "end")
+            {
+                var line = input.Split("->".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                
+                var sentence = line[0].Trim().Split();
+                var values = line[1].Trim().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i].Contains("{") || sentence[i].Contains("}"))
+                    {
+                        var placeHolderOne = sentence[i].IndexOf("{");
+                        var placeHolderTwo = sentence[i].IndexOf("}");
+                        var digit = int.Parse(sentence[i].Substring(placeHolderOne + 1, placeHolderTwo - 1));
+                        var textToReplace = sentence[i].Substring(placeHolderOne, placeHolderTwo + 1);
+                        if (digit < values.Length)
+                        {
+                            sentence[i] = sentence[i].Replace(textToReplace, values[digit]);
+                        }
+                    }
+                }
+
+                Console.WriteLine(string.Join(" ", sentence));
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Strings and Text Processing/02. JSON Stringify/JSONStringify.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.JSON_Stringify
+{
+    public class Students
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<int> Grades { get; set; }
+
+        public static Students Read(string input)
+        {
+            var splitGrades = input.Split("->".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var grades = new List<int>();
+            if (splitGrades.Length > 1)
+            {
+                grades = splitGrades[1].Trim().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            }
+
+            var splitNameAndAge = splitGrades[0].Trim().Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string name = splitNameAndAge[0].Trim();
+            int age = int.Parse(splitNameAndAge[1].Trim());
+
+            return new Students
+            {
+                Name = name,
+                Age = age,
+                Grades = new List<int>(grades) 
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{{name:\"{Name}\",age:{Age},grades:[{string.Join(", ", Grades)}]}}";
+        }
+    }
+
+    public class JSONStringify
+    {
+        static void Main()
+        {
+            var students = new List<Students>();
+            var input = Console.ReadLine();
+
+            while (input != "stringify")
+            {
+
+                var student = Students.Read(input);
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"[{string.Join(",", students)}]");
+        }
+    }
+}
