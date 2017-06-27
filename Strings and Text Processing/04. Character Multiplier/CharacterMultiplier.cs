ffb08c923187c6ecd5154d8705d860302ccc4879
Shtereva <a.i.shtereva@gmail.com>
2017-06-27 19:03:08 +0300

    Programming Fundamentals with CSharp

---
+++ Strings and Text Processing/04. Character Multiplier/CharacterMultiplier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace _04.Character_Multiplier
+{
+    class CharacterMultiplier
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var str1 = input[0].ToCharArray();
+            var str2 = input[1].ToCharArray();
+            int sum = 0;
+            sum = CalculateSumOfChars(str1, str2);
+
+            Console.WriteLine(sum);
+
+        }
+
+        private static int CalculateSumOfChars(char[] str1, char[] str2)
+        {
+            int sum;
+            if (str1.Length <= str2.Length)
+            {
+                sum = str1
+                    .Select(x => (int)x)
+                    .Select((x, index) => str1[index] * str2[index])
+                    .Sum();
+
+                sum += str2
+                    .Skip(str1.Length)
+                    .Select(x => (int)x)
+                    .Sum();
+            }
+
+            else
+            {
+                sum = str2
+                    .Select(x => (int)x)
+                    .Select((x, index) => str2[index] * str1[index])
+                    .Sum();
+
+                sum += str1
+                    .Skip(str2.Length)
+                    .Select(x => (int)x)
+                    .Sum();
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Strings and Text Processing/05. Magic exchangeable words/MagicExchangeableWords.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.Magic_exchangeable_words
+{
+    class MagicExchangeableWords
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            bool isReplacable = true;
+
+            var str1 = input[0].ToCharArray();
+            var str2 = input[1].ToCharArray();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = i + 1; j < str1.Length; j++)
+                {
+                    if (j >= str2.Length)
+                    {
+                        var letters = str1.Skip(str2.Length).ToArray();
+                        str1 = str1.Take(str2.Length).ToArray();
+
+                        for (int k = 0; k < letters.Length; k++)
+                        {
+                            if (!str1.Contains(letters[k]))
+                            {
+                                Console.WriteLine("false");
+                                return;
+                            }
+                        }
+
+                        if (isReplacable)
+                        {
+                            Console.WriteLine("true");
+                            return;
+                        }
+                    }
+                    CheckPattern(ref isReplacable, str1, str2, i, j);
+
+                    if (!isReplacable)
+                    {
+                        Console.WriteLine("false");
+                        return;
+                    }
+                }
+            }
+
+            if (str1.Length < str2.Length)
+            {
+                var letters = str2.Skip(str1.Length).ToArray();
+                str2 = str2.Take(str1.Length).ToArray();
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (!str2.Contains(letters[i]))
+                    {
+                        Console.WriteLine("false");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("true");
+        }
+
+        private static void CheckPattern(ref bool isReplacable, char[] str1, char[] str2, int i, int j)
+        {
+            if (str1[i] == str1[j])
+            {
+                if (str2[i] != str2[j])
+                {
+                    isReplacable = false;
+                }
+            }
+
+            else
+            {
+                if (str2[i] == str2[j])
+                {
+                    isReplacable = false;
+                }
+            }
+
+        }
+    }
+}
